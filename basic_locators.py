5fb17ec7ebc855d699898e5ebffbcca33bc81884
ATarasenko-sudo <andrey.tarasenko.00@list.ru>
2021-03-29 09:04:00 +0300

    Исправлен локатор с составным классом

--- basic_locators.py
+++ basic_locators.py
@@ -17,6 +17,6 @@ PHONE_ROW_PROFILE_INF_LOCATOR = ("//div[@class='js-contacts-field-phone profile_
 EMAIL_ROW_PROFILE_INF_LOCATOR = ( "//div[@class='js-additional-email profile__list__row__input']//input[@class='input__inp js-form-element']")
 SECOND_EMAIL_ROW_PROFILE_INF_LOCATOR = ("//div[@data-email-index=1]//input[@class='input__inp js-form-element']")
 ADD_SECOND_EMAIL_BUTTON = ( "//div[@class='clickable-button clickable-button_email_add clickable-button_simple']//div[@class='clickable-button__spinner js-button-spinner']")
-SUBMIT_BUTTON_LOCATOR = ( "//button[@class='button button_submit']")
+SUBMIT_BUTTON_LOCATOR = ( "//div[@class='button__text']")
 
 
