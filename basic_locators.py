e89fb0ecd8620a478d7d6a78ede727543508a26c
ATarasenko-sudo <79630297+ATarasenko-sudo@users.noreply.github.com>
2021-03-30 00:15:01 +0300

    Домашнее задание №1 (#1)
    * Selenium basic skills
    
    * Add files via upload
    
    * Заменены локаторы с "хвостом" на другие
    
    * fix merge conflict
    
    * Исправлен локатор с составным классом
    
    * Исправлен локатор с составным классом и длинным адресом

---
+++ basic_locators.py
@@ -0,0 +1,22 @@
+
+AUTH_RU_BUTTON_LOCATOR = ('//div[contains(text(), "Войти")]')
+AUTH_EN_BUTTON_LOCATOR = ('//div[contains(text(), "Log In")]')
+CHECK_LANGUAGE_LOCATOR = ('//div[contains(text(), "En")]')
+AUTH_EMAIL_ROW_LOCATOR = ('email')
+AUTH_PASSWOR_ROW_LOCATOR = ('password')
+LOGGIN_RU_BUTTON_LOCATOR = ('//div[@class = "js-target-common-modals"]//div[contains(text(), "Войти")]')
+LOGGIN_EN_BUTTON_LOCATOR = ('//div[@class = "js-target-common-modals"]//div[contains(text(), "Log In")]')
+
+
+USER_INF_LOCATOR = ('js-head-balance')
+LOGUT_BUTTON_LOCATOR = ('//a[@href="/logout"]')
+
+PROFILE_BUTTON_MENU_LOCATOR = ('//a[@href="/profile"]')
+NAME_ROW_PROFILE_INF_LOCATOR = ( "//div[@class='js-contacts-field-name profile__list__row__input']//input[@class='input__inp js-form-element']")
+PHONE_ROW_PROFILE_INF_LOCATOR = ("//div[@class='js-contacts-field-phone profile__list__row__input']//input[@class='input__inp js-form-element']")
+EMAIL_ROW_PROFILE_INF_LOCATOR = ( "//div[@class='js-additional-email profile__list__row__input']//input[@class='input__inp js-form-element']")
+SECOND_EMAIL_ROW_PROFILE_INF_LOCATOR = ("//div[@data-email-index=1]//input[@class='input__inp js-form-element']")
+ADD_SECOND_EMAIL_BUTTON = ( "//div[@class='clickable-button clickable-button_email_add clickable-button_simple']//div[@class='clickable-button__spinner js-button-spinner']")
+SUBMIT_BUTTON_LOCATOR = ( "//div[@class='button__text']")
+
+
